2c55cff632e2d2da366013e6d949709d0b811d4e
Guido Espinoza <guidoespinozac@gmail.com>
2025-07-07 19:47:29 -0400

    feat: actualizar enlace del CV en la sección Hero

--- app/components/sections/hero/HeroSection.tsx
+++ app/components/sections/hero/HeroSection.tsx
@@ -60,7 +60,7 @@ const HeroSection = () => {
                             </div>
                         </Link>
 
-                        <Link className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold" role="button" target="_blank" href='https://wrdc7yopavc4jezh.public.blob.vercel-storage.com/Curriculum-Guido-Espinoza-iroBY8SBY5Ke0z23F88YRV6X1z71Pr.pdf'
+                        <Link className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold" role="button" target="_blank" href='https://wrdc7yopavc4jezh.public.blob.vercel-storage.com/CV_Guido_Espinoza-QqSgllN7gj2JkN5oirIl49pfp1PTAA.pdf'
                         >
                             <span>Obtener CV</span>
                             <MdDownload size={16} />
